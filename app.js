707c460f86491fd31a9ec6f64accb0af03d73dd3
Ilhamdocs <100422967+Ilhamdocs@users.noreply.github.com>
2022-07-12 10:45:40 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+let konfir = confirm("Ok kmu Harus Mencintai ku.!")
+if (konfir === true) {
+  let nama = prompt("Siapa Nama Kmu?")
+  alert(`Hai ${nama} Kmu kok bisa Cinta Aku sihh><`)
+  alert (`Sumpah ya ${nama} Kmu tuh bikin aku baper Tau ga `)
+}else{
+  document.writeln("Biar apa kek gitu sihhh")
+  document.writeln("Aku ga suka ya klo di batalin gitu")
+  
+}
